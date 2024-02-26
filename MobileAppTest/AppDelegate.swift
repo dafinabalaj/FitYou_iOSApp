c332f8903dd941be9d40bf4aaad00bf5f4849932
dafinabalaj <dafinabalaj345@gmail.com>
2024-02-26 16:21:43 +0100

    Database Setup

--- MobileAppTest/AppDelegate.swift
+++ MobileAppTest/AppDelegate.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import SQLite3
+
+
+var databasePointer: OpaquePointer?
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +19,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if let dbPointer = DBHelper.getDatabasePointer(databaseName: "MobileAppDatabase.db"){
+            databasePointer = dbPointer
+        }else{
+            // do something
+        }
         return true
     }
 
---
+++ MobileAppTest/Database/DBHelper.swift
@@ -0,0 +1,71 @@
+//
+//  DBHelper.swift
+//  MobileAppTest
+//
+//  Created by THIS on 26.2.24.
+//  Copyright © 2024 THIS. All rights reserved.
+//
+
+import Foundation
+import SQLite3
+
+class DBHelper {
+    static func getDatabasePointer(databaseName: String) -> OpaquePointer? {
+        
+        var databasePointer: OpaquePointer?
+        
+        
+        let documentDatabasePath = FileManager.default.urls(for: .documentDirectory, in:
+            .userDomainMask)[0].appendingPathComponent(databaseName).path
+        
+        if FileManager.default.fileExists(atPath: documentDatabasePath){
+            print("Database Exists (already)")
+        }
+        else {
+            
+            guard let bundleDatabasePath = Bundle.main.resourceURL?.appendingPathComponent(databaseName).path else{
+                print("Unwrapping Error: Bundle Database Path doesn't exist")
+                return nil
+            }
+            
+            do{
+                try FileManager.default.copyItem(atPath: bundleDatabasePath, toPath: documentDatabasePath)
+                print("Database created (copied)")
+            }catch{
+                print("Error: \(error.localizedDescription)")
+                return nil
+            }
+        }
+        
+        if sqlite3_open(documentDatabasePath, &databasePointer)== SQLITE_OK{
+            print("Successfully open database")
+            print("Database path: \(documentDatabasePath)")
+        }
+        else{
+            print("Could not open database")
+            
+        }
+        return databasePointer
+    }
+    
+    static func closeDatabase(dbPointer: OpaquePointer?) {
+        guard let db = dbPointer else {
+            print("Database pointer is already nil")
+            return
+        }
+
+        if sqlite3_close(db) == SQLITE_OK {
+            print("Database closed successfully.")
+        } else {
+            let errorMessage = String(cString: sqlite3_errmsg(db))
+            print("Failed to close database. Error: \(errorMessage)")
+        }
+    }
+
+    static func getDatabasePath(databaseName: String) -> String? {
+            if let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                let databasePath = documentDirectory.appendingPathComponent(databaseName).path
+                return databasePath
+            }
+            return nil
+        }}
